6cbf61753595439ea717e702e4a36f5d91450427
rrayas <robert.rayas@cccs.edu>
2019-05-23 13:01:59 -0600

    Section 4 - add db

--- src/main/java/com/boot/model/Shipwreck.java
+++ src/main/java/com/boot/model/Shipwreck.java
@@ -1,6 +1,17 @@
 package com.boot.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Shipwreck {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	Long id;
 	String name;
 	String description;
